951c2629da779f6324b72db372799bd7d6f1a300
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-05-11 16:19:27 -0700

    Implement identity and test

--- test/underbar/identity.test.js
+++ test/underbar/identity.test.js
@@ -3,17 +3,21 @@ const _ = require('../../underbar');
 describe('identity()', () => {
   it('returns null if given null', () => {
     // Your code goes here
+    expect(_.identity(null)).toBe(null);
   });
 
   it('returns the number if given a number', () => {
     // Your code goes here
+    expect(_.identity(2)).toBe(2);
   });
 
   it('returns the same array if given an array', () => {
     // Your code goes here
+    expect(_.identity([1, 2])).toEqual([1, 2]);
   });
 
   it('returns the same object if given an object', () => {
     // Your code goes here
+    expect(_.identity({'name': 'Ben', age: 26})).toEqual({'name': 'Ben', age: 26});
   });
 });
