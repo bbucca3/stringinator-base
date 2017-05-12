0231b512432091acf0124f424fb691fe94baf05b
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-05-11 22:25:36 -0700

    Implement pluck func and test

--- test/underbar/pluck.test.js
+++ test/underbar/pluck.test.js
@@ -3,6 +3,13 @@ const _ = require('../../underbar');
 describe('pluck()', () => {
   it('returns an array of just ages, given an array of people objects', () => {
     // Your code goes here
+    let people = [
+    {name: "Ben", age: 26},
+    {name: "Ben", age: 27},
+    {name: "Ben", age: 34}
+    ];
+
+    expect(_.pluck(people, "age")).toEqual([26, 27, 34]);
   });
 
 });
